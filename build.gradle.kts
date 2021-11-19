90b65d28c094e241e9209740e12d66c49d15dceb
cssxsh <cssxsh@gmail.com>
2021-11-20 00:00:41 +0800

    build: 1.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "io.github.gnuf0rce"
-version = "1.0.3"
+version = "1.1.0"
 
 repositories {
     mavenLocal()
--- src/main/kotlin/io/github/gnuf0rce/mirai/NetdiskFileSyncPlugin.kt
+++ src/main/kotlin/io/github/gnuf0rce/mirai/NetdiskFileSyncPlugin.kt
@@ -11,7 +11,7 @@ object NetdiskFileSyncPlugin : KotlinPlugin(
     JvmPluginDescription(
         id = "io.github.gnuf0rce.file-sync",
         name = "file-sync",
-        version = "1.0.3",
+        version = "1.1.0",
     ) {
         author("cssxsh")
     }
